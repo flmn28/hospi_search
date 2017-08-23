caea6cd58f76b48409846c7f9fc41a0f583080fb
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-08-23 16:36:28 +0900

    locationsテーブルからdistrictカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822153811) do
+ActiveRecord::Schema.define(version: 20170823073505) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -44,78 +43,6 @@ ActiveRecord::Schema.define(version: 20170822153811) do
   create_table "locations", force: :cascade do |t|
     t.string   "prefecture"
     t.string   "city"
-    t.string   "district"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "patients", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "family_name_kanji"
-    t.string   "first_name_kanji"
-    t.string   "family_name_kana"
-    t.string   "first_name_kana"
-    t.boolean  "gender"
-    t.date     "birthday"
-    t.integer  "location_id"
-    t.index ["email"], name: "index_patients_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
-  end
-
-end
-=======
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20170823050503) do
-
-  create_table "departments", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "hospitals", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "image"
-    t.integer  "location_id"
-    t.index ["email"], name: "index_hospitals_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_hospitals_on_reset_password_token", unique: true
-  end
-
-  create_table "hospitals_departments", force: :cascade do |t|
-    t.integer  "hospital_id"
-    t.integer  "department_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "prefecture"
-    t.string   "city"
-    t.string   "district"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -149,4 +76,3 @@ ActiveRecord::Schema.define(version: 20170823050503) do
   end
 
 end
->>>>>>> c203ee9fcabfc23b05977e28459308b4e265ecf0
