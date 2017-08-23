d616f732b593db0f9e40dc4342a63a2c6853f714
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-08-23 21:46:33 +0900

    PatientとLocation間のリレーションを一旦解除

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,6 @@ class Patient < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :location
+  # belongs_to :location
   has_many :reservations
 end
