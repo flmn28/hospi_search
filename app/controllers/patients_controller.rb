5e0e63f4f53d00fa8ccb74b4de5f0c216fc21563
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-08-24 22:58:06 +0900

    マイページでログインしているユーザーの情報とその予約状況を表示

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,11 @@
 class PatientsController < ApplicationController
   def show
+    @reservations = current_patient.reservations
+
+    if current_patient.gender
+      @gender = '男'
+    else
+      @gender = '女'
+    end
   end
 end
