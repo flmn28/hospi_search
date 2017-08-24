2a9a2908a43130da3f1398689e009813c61abf63
Joichiro Hashikawa <j.hashikawa.tech@gmail.com>
2017-08-25 01:05:19 +0900

    レビュー投稿機能を実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,12 @@
 class ReviewsController < ApplicationController
+
   def new
+    @hospital = Hospital.find_by(id: params[:hospital_id])
+  end
+
+  def create
+    @review = Review.create(name: params[:name], comment: params[:comment], hospital_id: params[:hospital_id])
+    redirect_to hospital_reviews_path(params[:hospital_id].to_i)
   end
 
   def review_list
